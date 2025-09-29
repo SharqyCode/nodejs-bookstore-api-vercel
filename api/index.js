752ed6d4a814c963af4570b14a7eb00bc7799028
ITI050 <ITI050@isi.net>
2025-09-28 23:08:53 -0700

    preparing for vercel

---
+++ api/index.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const app = require("../src/app"); // import your existing app.js
+
+module.exports = app;
---
+++ src/noServer.js
@@ -0,0 +1,13 @@
+// require('dotenv').config({path:"../.env"});
+// const mongoose = require('mongoose');
+// const app = require('./app');
+// const config = require('./config');
+// const connectDB = require('./config/db');
+
+
+// connectDB().then((res)=>{
+//     console.log(res);
+//     app.listen(config.port, ()=>{
+//         console.log(`Server running on: http://localhost:${config.port}/`);
+//     })
+// })
