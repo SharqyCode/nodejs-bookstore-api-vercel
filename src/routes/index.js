e0084d20ae2be695101ce38dfe7e38c691ca3eb0
Ebraheem <pencilmagicart@gmail.com>
2025-10-11 08:24:47 +0300

    added homepage with \n

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,33 +9,29 @@ router.use("/api/books", bookRouter);
 router.use("/api/authors", authorRouter);
 router.use("/api/u", userRouter);
 router.use("/", (req, res) => {
-    res.send(
-        `
-        === BOOk Store API ===\n
-        \n
-        \n
-        ---Books--- 
-        GET /api/books --> get all books\n
-        GET /api/books/{id} --> get book by ID\n
-        POST /api/books/addBook --> add book to DB\n
-        PATCH /api/books/update/{id} --> edit book & save to DB\n
-        DELETE /api/books/delete/{id} --> delete book from DB\n
-        \n
-        \n
-        ---Authors---\n  
-        GET /api/authors --> get all authors\n
-        GET /api/authors/{id} --> get author by ID\n
-        POST /api/authors/ --> add author to DB\n
-        PUT /api/authors/{id} --> edit author & save to DB\n
-        DELETE /api/authors/{id} --> delete author from DB\n
-        \n
-        \n
-        ---Users---\n
-        POST /api/u/register --> add user to DB\n
-        POST /api/u/login --> login with saved credentials\n
-        GET /api/u/me --> visit user profile (must send JWT in request header)\n
+    res.send(`
+    <h2>=== Book Store API ===</h2>
+    <p>
+    <strong>--- Books ---</strong><br>
+    GET /api/books → get all books<br>
+    GET /api/books/{id} → get book by ID<br>
+    POST /api/books/addBook → add book to DB<br>
+    PATCH /api/books/update/{id} → edit book & save to DB<br>
+    DELETE /api/books/delete/{id} → delete book from DB<br><br>
 
-        `)
-})
+    <strong>--- Authors ---</strong><br>
+    GET /api/authors → get all authors<br>
+    GET /api/authors/{id} → get author by ID<br>
+    POST /api/authors/ → add author to DB<br>
+    PUT /api/authors/{id} → edit author & save to DB<br>
+    DELETE /api/authors/{id} → delete author from DB<br><br>
+
+    <strong>--- Users ---</strong><br>
+    POST /api/u/register → add user to DB<br>
+    POST /api/u/login → login with saved credentials<br>
+    GET /api/u/me → visit user profile (must send JWT in header)
+    </p>
+  `);
+});
 
 module.exports = router;
