1d9413917a59bb4317b03e983fb412adaf96b173
Ebraheem <pencilmagicart@gmail.com>
2025-10-11 08:20:28 +0300

    added homepage with \n

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,26 +11,29 @@ router.use("/api/u", userRouter);
 router.use("/", (req, res) => {
     res.send(
         `
-        ---Books---  
-        GET /api/books --> get all books
-        GET /api/books/{id} --> get book by ID
-        POST /api/books/addBook --> add book to DB
-        PATCH /api/books/update/{id} --> edit book & save to DB
-        DELETE /api/books/delete/{id} --> delete book from DB
-        
-
-        ---Authors---  
-        GET /api/authors --> get all authors
-        GET /api/authors/{id} --> get author by ID
-        POST /api/authors/ --> add author to DB
-        PUT /api/authors/{id} --> edit author & save to DB
-        DELETE /api/authors/{id} --> delete author from DB
-        
-        
-        ---Users---
-        POST /api/u/register --> add user to DB
-        POST /api/u/login --> login with saved credentials
-        GET /api/u/me --> visit user profile (must send JWT in request header)
+        === BOOk Store API ===\n
+        \n
+        \n
+        ---Books--- 
+        GET /api/books --> get all books\n
+        GET /api/books/{id} --> get book by ID\n
+        POST /api/books/addBook --> add book to DB\n
+        PATCH /api/books/update/{id} --> edit book & save to DB\n
+        DELETE /api/books/delete/{id} --> delete book from DB\n
+        \n
+        \n
+        ---Authors---\n  
+        GET /api/authors --> get all authors\n
+        GET /api/authors/{id} --> get author by ID\n
+        POST /api/authors/ --> add author to DB\n
+        PUT /api/authors/{id} --> edit author & save to DB\n
+        DELETE /api/authors/{id} --> delete author from DB\n
+        \n
+        \n
+        ---Users---\n
+        POST /api/u/register --> add user to DB\n
+        POST /api/u/login --> login with saved credentials\n
+        GET /api/u/me --> visit user profile (must send JWT in request header)\n
 
         `)
 })
