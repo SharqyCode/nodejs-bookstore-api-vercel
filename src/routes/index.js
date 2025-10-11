54ef111a373d626cdccbc3915088e6cf6fb586d2
Ebraheem <pencilmagicart@gmail.com>
2025-10-11 08:16:56 +0300

    added homepage

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,8 +8,31 @@ const router = express.Router();
 router.use("/api/books", bookRouter);
 router.use("/api/authors", authorRouter);
 router.use("/api/u", userRouter);
-router.use("/api", (req,res)=>{
-    res.send("WELCOME")
+router.use("/", (req, res) => {
+    res.send(
+        `
+        ---Books---  
+        GET /api/books --> get all books
+        GET /api/books/{id} --> get book by ID
+        POST /api/books/addBook --> add book to DB
+        PATCH /api/books/update/{id} --> edit book & save to DB
+        DELETE /api/books/delete/{id} --> delete book from DB
+        
+
+        ---Authors---  
+        GET /api/authors --> get all authors
+        GET /api/authors/{id} --> get author by ID
+        POST /api/authors/ --> add author to DB
+        PUT /api/authors/{id} --> edit author & save to DB
+        DELETE /api/authors/{id} --> delete author from DB
+        
+        
+        ---Users---
+        POST /api/u/register --> add user to DB
+        POST /api/u/login --> login with saved credentials
+        GET /api/u/me --> visit user profile (must send JWT in request header)
+
+        `)
 })
 
 module.exports = router;
