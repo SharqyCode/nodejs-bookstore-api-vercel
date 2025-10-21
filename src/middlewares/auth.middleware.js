b7ee9ee8c0492ee2b68954815ff9fde7f0ddc702
Ebraheem <pencilmagicart@gmail.com>
2025-10-22 01:40:52 +0300

    boo

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,21 +1,27 @@
 // const jwt = require("jsonwebtoken");
-const {verifyToken} = require("../config/jwt")
-const config = require("../config");
+const { verifyToken } = require("../config/jwt")
 const { getUserById } = require("../services/user.service");
 const User = require("../models/user.model")
 
 const authMiddleware = async (req, res, next) => {
     try {
         const authHeader = req.headers.authorization;
+        console.log("Auth header", req.headers);
+
 
         if (!authHeader || !authHeader.startsWith("Bearer ")) {
-            throw new Error("No token provided");
+            throw "No token provided";
+
         }
 
         const token = authHeader.split(" ")[1];
 
         const decoded = verifyToken(token);
-        const user = await User.findOne({ userId: decoded.id }, { password: 0 })
+        console.log("decoded", decoded);
+
+        const user = await User.findOne({ userId: decoded.userId }, { password: 0 })
+        if (!user)
+            throw "user doesn't exist for token provided"
         req.user = user;
         next();
     } catch (err) {
@@ -23,4 +29,14 @@ const authMiddleware = async (req, res, next) => {
     }
 };
 
-module.exports = authMiddleware;
+const adminOnly = (req, res, next) => {
+    if (req.user.role !== "admin") {
+        return res.status(403).json({
+            status: "FORBIDDEN",
+            message: "Admins only",
+        });
+    }
+    next();
+};
+
+module.exports = { authMiddleware, adminOnly };
